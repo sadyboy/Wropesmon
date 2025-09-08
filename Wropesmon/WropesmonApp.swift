9879776b579b03028a71f91d9bbe866389af3294
Serhii Anp <sadyboy.sa@gmail.com>
2025-09-08 13:46:43 +0300

    Initial commit

---
+++ Wropesmon/WropesmonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WropesmonApp.swift
+//  Wropesmon
+//
+//  Created by Serhii Anp on 08.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct WropesmonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
